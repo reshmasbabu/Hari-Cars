d01b7f03f2f1c649bf0f64b74c4affe27cd231b0
Reshma Suresh Babu <113236505+reshmasbabu@users.noreply.github.com>
2023-10-31 23:48:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,25 @@ import pandas as pd
 excel_file = 'data.xlsx'
 df = pd.read_excel(excel_file)
 
-# Streamlit app
-st.title("Find My Car from Hari's Collections")
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://unsplash.com/photos/die-cast-car-collection-on-rack-JBrfoV-BZts");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+
+add_bg_from_url()
+
+
+st.markdown("<h1 style='color: red;'>Find My Car from Hari's Collections</h1>", unsafe_allow_html=True)
 
 # Create a search box
 search_query = st.text_input("Enter the car name:")
@@ -14,5 +31,7 @@ search_query = st.text_input("Enter the car name:")
 # Filter the DataFrame based on the search query
 filtered_df = df[df['Name'].str.contains(search_query, case=False, na=False)]
 
-# Display the filtered results
-st.dataframe(filtered_df)
+import streamlit as st
+
+# Display the filtered results with custom width and height
+st.dataframe(filtered_df, width=400, height=0)
